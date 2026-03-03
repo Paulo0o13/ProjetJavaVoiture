aded087104ab648493f40b39886befcb5a9178aa
Paul BUFFARD <paulbuffard13@gmail.com>
2026-03-03 20:53:16 +0100

    commit

--- src/main/java/org/example/controller/CarsController.java
+++ src/main/java/org/example/controller/CarsController.java
@@ -11,24 +11,32 @@ import java.util.List;
 @Controller
 public class CarsController {
 
-
     private final CarRepository carRepository;
 
     public CarsController(CarRepository carRepository) {
         this.carRepository = carRepository;
     }
 
+    // 1. Affiche UNIQUEMENT le formulaire sur /car
     @GetMapping("/car")
-    public String findAllCars(Model model) {
-        List<Car> cars = this.carRepository.findAll();
-        model.addAttribute("cars", cars);
+    public String showForm(Model model) {
         model.addAttribute("car", new Car());
         return "formCar";
     }
 
+    // 2. Sauvegarde et redirige vers la liste des voitures (/cars)
     @PostMapping("/car")
     public String addOneCar(@ModelAttribute("car") Car car) {
         this.carRepository.save(car);
-        return "redirect:/car";
+        // On redirige vers la nouvelle URL
+        return "redirect:/cars";
+    }
+
+    // 3. Affiche UNIQUEMENT la liste sur /cars
+    @GetMapping("/cars")
+    public String findAllCars(Model model) {
+        List<Car> cars = this.carRepository.findAll();
+        model.addAttribute("cars", cars);
+        return "listCar";
     }
 }
