ace3161c73a064cb84eb118e5358bb0db68987e5
Paul BUFFARD <paulbuffard13@gmail.com>
2026-03-09 22:55:10 +0100

    commit

--- src/main/java/org/example/controller/TransactionController.java
+++ src/main/java/org/example/controller/TransactionController.java
@@ -24,19 +24,17 @@ public class TransactionController {
      *
      * */
     @PostMapping("/buy-vehicle") // Remplace ton /sendMessage
-    public ResponseEntity<?> buyVehicle(@RequestParam("carId") Long carId,
+    public ResponseEntity<?> buyVehicle(@RequestParam("iban") String iban,
                                         @RequestParam("price") double price,
                                         @RequestParam("type") String type) {
 
-        // On prépare la requête selon le Step 2 du PDF [cite: 31]
+
         Map<String, Object> request = Map.of(
-                "userId", "PseudoDeLutlisateur", // Récupère ton utilisateur connecté
-                "amount", price,                 // Le prix du véhicule
-                "operationType", type,           // "rent" ou "buy"
-                "carId", carId                   // Pour s'en souvenir au retour
+                "userId", iban,
+                "amount", price,
+                "operationType", type
         );
 
-        // On envoie vers la file de REQUÊTE [cite: 67]
         rabbitTemplate.convertAndSend(RabbitConfiguration.REQUEST_QUEUE, request);
 
         return ResponseEntity.ok(Map.of("status", "Verification en cours..."));
