279516315834e051ba7fbbef394be07cfdb6e2a9
Paul BUFFARD <paulbuffard13@gmail.com>
2026-03-12 10:27:53 +0100

    pom

--- src/main/java/org/example/configuration/WebSocketConfig.java
+++ src/main/java/org/example/configuration/WebSocketConfig.java
@@ -1,5 +1,7 @@
 package org.example.configuration;
 
+import org.example.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -9,6 +11,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
